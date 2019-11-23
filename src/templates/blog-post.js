e61618fa521aaa30a64a6a414986321d6bc6060a
Quoc Bao Nguyen <punkupoz@gmail.com>
2019-11-23 13:31:01 +1000

    add twitter discuss

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -176,11 +176,10 @@ class BlogPostTemplate extends React.Component {
             <div dangerouslySetInnerHTML={{ __html: html }} />
             <footer>
               <p>
-                {/* <a href={discussUrl} target="_blank" rel="noopener noreferrer">
+                <a href={discussUrl} target="_blank" rel="noopener noreferrer">
                   Discuss on Twitter
-                </a> */}
+                </a>
                 {` • `}
-                You can help me improve this blog by&nbsp;
                 <a href={editUrl} target="_blank" rel="noopener noreferrer">
                   Edit on GitHub
                 </a>
